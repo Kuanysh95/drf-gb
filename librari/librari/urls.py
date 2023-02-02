9f0cefffe587296be87b8d83e9f8404de55b931d
Kuanysh <anuarbekov.kuanysh@mail.ru>
2023-02-02 21:49:08 +0600

    lesson4

--- librari/librari/urls.py
+++ librari/librari/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from authors.views import AuthorModelViewSet, ArticleModelViewSet, BookModelViewSet, BiographyModelViewSet
-
+from authors.views import AuthorModelViewSet, ArticleModelViewSet, BookModelViewSet, BiographyModelViewSet, MyAPIView
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
@@ -14,4 +13,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('myapi/', MyAPIView.as_view({'get': 'list'})),
 ]
